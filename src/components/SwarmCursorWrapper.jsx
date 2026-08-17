aa7f7bb543955ee55bacc40df6c35e89a604a954
Raghul <raghulfp@gmail.com>
2026-08-17 18:11:39 +0530

    chore: remove unused components and assets

--- src/components/SwarmCursorWrapper.jsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic';
-
-const SwarmCursor = dynamic(() => import('./SwarmCursor'), { ssr: false });
-
-export default function SwarmCursorWrapper(props) {
-  return <SwarmCursor {...props} />;
-}
